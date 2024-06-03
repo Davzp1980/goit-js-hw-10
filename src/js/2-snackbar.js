958907caa82658e406e4f74ceb703856ee718140
Oleksii Donets <108811517+Davzp1980@users.noreply.github.com>
2024-06-03 13:10:32 +0300

    done

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,47 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const formElem = document.querySelector('.form');
+
+formElem.addEventListener('submit', e => {
+  e.preventDefault();
+  const delay = formElem.elements.delay.value;
+  const state = formElem.elements.state.value;
+
+  const newPromise = delay => {
+    return new Promise((resolve, reject) => {
+      if (state === 'fulfilled') {
+        setTimeout(() => {
+          resolve(fulfilled(delay));
+        }, delay);
+      } else {
+        setTimeout(() => {
+          reject(rejected(delay));
+        }, delay);
+      }
+    });
+  };
+
+  newPromise(delay)
+    .then(resolve => resolve)
+    .catch(reject => reject);
+  formElem.reset();
+});
+
+function fulfilled(delayNum) {
+  return iziToast.show({
+    iconUrl: './public/bi_check2-circle.svg',
+    backgroundColor: '#59a10d',
+    message: `Fulfilled promise in ${delayNum}ms`,
+    position: 'topCenter',
+  });
+}
+
+function rejected(delayNum) {
+  return iziToast.show({
+    iconUrl: './public/bi_x-octagon.svg',
+    backgroundColor: '#ef4040',
+    message: `Rejected promise in ${delayNum}ms`,
+    position: 'topCenter',
+  });
+}
